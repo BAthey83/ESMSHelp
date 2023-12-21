4e3861d8f7c8873fc94c5ba3ea2dec228122a1c9
Chris Boler <christopher.a.boler@erdc.dren.mil>
2023-12-21 16:47:26 -0600

    Update build.ps1

--- Pipelines/build.ps1
+++ Pipelines/build.ps1
@@ -7,3 +7,13 @@
 madbuild -project ".\ESMS.flprj" -target "ESMS-Web-Reorg-2023"
 
 madbuild -project ".\ESMS.flprj" -target "ESMS-PDF-Reorg-2023"
+
+if (Test-Path $ARTIFACT_PATH) { New-Item $ARTIFACT_PATH -ItemType "directory" }
+
+$outputDirectories = ".\Output\Gitlab-Runner\"
+
+$subdirectories = Get-ChildItem -Path $outputDirectories -Directory
+
+foreach ($subdirectory in $subdirectories) {
+    Copy-Item -Path $subdirectory.FullName -Destination $ARTIFACT_PATH -Recurse
+}
